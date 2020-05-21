8a63a454586e76dedfa0e6bdddd2e6101277e9d0
FYH7Yuan <49849050+FYH7Yuan@users.noreply.github.com>
2020-05-21 20:16:49 +0800

    copyright

--- src/components/HomeCop/OtherFile.vue
+++ src/components/HomeCop/OtherFile.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>
-        <div class="_kt">块状填充物</div>
-        <div class="_kt">块状填充物</div></div>
+  <div>
+    <div class="_kt">块状填充物</div>
+    <div class="_kt">
+      <p>Copyright 2020</p>
+      <a href="http://www.beian.miit.gov.cn/">京ICP备20009530号</a>
+      <p>免责声明</p>
+    </div>
+  </div>
 </template>
-<style scoped>
-    div{
-        height: 400px;
-        line-height: 400px;
-        background-color: #3086f2;
-        margin-top: 20px;
-    }
+<style lang="scss" scoped>
+* {
+  text-decoration: none;
+}
+._kt {
+  height: 400px;
+  background-color: #3086f2;
+  margin-top: 20px;
+  text-align: center;
+  a,
+  p {
+    height: 20px;
+    line-height: 20px;
+    color: #fff;
+    margin-top: 20px;
+  }
+  a{
+      display: block;
+      &:hover{
+          color:#000;
+      }
+  }
+}
 </style>
